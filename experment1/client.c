5d93c83182a253c63d7c7e42287dd0a970db03e3
Frayn-QN <1586622092@qq.com>
2023-06-12 23:14:59 +0800

    null

---
+++ experment1/client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <netdb.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
