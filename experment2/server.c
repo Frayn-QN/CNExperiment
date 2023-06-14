414158630c296836378ccbf351f798690b853f18
Frayn-QN <1586622092@qq.com>
2023-06-15 00:37:56 +0800

    except server_func

--- experment2/server.c
+++ experment2/server.c
@@ -16,19 +16,41 @@
 
 int sigint_flag = 0;
 void handle_sigint(int sig) {
-    printf("[srv] SIGINT is coming!\n");
+    printf("[srv](%d) SIGINT is coming!\n", getpid());
     sigint_flag = 1;
 }
 
 void handle_sigchld(int sig) {
-    pid_t pid_chld;
+    pid_t child_pid;
+    pid_t main_pid = getpid();
     int stat;
-    while((pid_chld = waitpid(-1, &stat, WNOHANG)) > 0);
-    printf("[srv](%d)[chd](%d) Child has terminated!\n", getppid(), getpid());
+    while((child_pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+        printf("[srv](%d)[chd](%d) Child has terminated!\n", main_pid, child_pid);
+    }
 }
 
-void server_func(int connfd, const char* vcd) {
+void server_func(int connfd, int vcd, pid_t pid) {
+    while(1) {
+        char buf[MAXLINE] = {0};
 
+        // 接收消息
+        int valread = read(connfd, buf, MAXLINE);
+        if(valread <= 0) {
+            if (valread == -1){
+                perror("read error");
+            }
+            break;
+        }
+        printf("[ECH_RQT]%s", buf);
+
+        // 发送回声
+        char rep[MAXLINE] = {0};
+        sprintf(rep, "(%s)%s", vcd, buf);
+        if(write(connfd, rep, strlen(rep)) == -1) {
+            perror("write error");
+            break;
+        }
+    }
 }
 
 
@@ -42,6 +64,8 @@ int main(int argc, char** argv) {
     char* server_ip = argv[1];
     char* server_port = argv[2];
     char* server_vcd = argv[3];
+    pid_t main_pid = getpid();
+    pid_t child_pid;
 
     // 安装SIGINT信号处理器
     struct sigaction sa;
@@ -56,6 +80,11 @@ int main(int argc, char** argv) {
     sigemptyset(&sa.sa_mask);
     sigaction(SIGCHLD, &sa, NULL);
 
+    // 安装SIGPIPE信号处理器
+    sa.sa_flags = 0;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa,sa_mask);
+    sigaction(SIGPIPE, &sa, NULL);
 
     // 设置服务器地址
     struct sockaddr_in server_addr;
@@ -86,7 +115,7 @@ int main(int argc, char** argv) {
         perror("listen error");
         return 1;
     }
-    printf("[srv] server[%s:%s][%s] is initializing!\n", server_ip, server_port, server_vcd);
+    printf("[srv](%d)[srv_sa](%s:%s)[vcd](%s) Server has initialized!\n", server_ip, server_port, server_vcd);
 
     // 受理业务
     while(!sigint_flag) {
@@ -111,16 +140,34 @@ int main(int argc, char** argv) {
             perror("inet_ntop error");
             return 1;
         }
+        int client_port = ntohs(client_addr.sin_port);
+        printf("[srv](%d)[cli_sa](%s:%d) Client is accepted!\n", main_pid, client_ip, client_port);
 
-        printf("[srv] client[%s:%d] is accepted!\n", client_ip, ntohs(client_addr.sin_port));
-        server_func(connfd, server_vcd);
-        printf("[srv] client[%s:%d] is closed!\n", client_ip, ntohs(client_addr.sin_port));
+        // 创建子进程处理客户端请求
+        child_pid = fork();
+        if(child_pid == -1) {
+            perror("fork error");
+            return 1;
+        }
+        else if(child_pid == 0) {// 子进程
+            printf("[chd](%d)[ppid](%d) Client process is created!\n", child_pid, main_pid);
+            close(listenfd);
+
+            server_func(connfd, atoi(server_vcd), child_pid);
+
+            printf("[chd](%d)[ppid](%d)[cli_sa](%s:%d) Client is closed!\n", child_pid, main_pid, client_ip, client_port);
+            close(connfd);
+            printf("[chd](%d)[ppid](%d) connfd is closed!\n", child_pid, main_pid);
+            printf("[chd](%d)[ppid](%d) Client process is to return!\n", child_pid, main_pid);
+            return 0;
+        }
+        
         close(connfd);
     }
     
     close(listenfd);
-    printf("[srv] listenfd is closed!\n");
-    printf("[srv] server is to return!\n");
+    printf("[srv](%d) listenfd is closed!\n", main_pid);
+    printf("[srv](%d) server is to return!\n", main_pid);
 
     return 0;
 }
